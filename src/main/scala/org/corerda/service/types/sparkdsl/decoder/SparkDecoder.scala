9af312d274132d4be61fe4c61def915e42d4691d
Adrian <adrianverdu1990@gmail.com>
2023-04-16 01:19:50 +0200

    added yaml decoder for spark implementation

---
+++ src/main/scala/org/corerda/service/types/sparkdsl/decoder/SparkDecoder.scala
@@ -0,0 +1,46 @@
+package org.corerda.service.types.sparkdsl.decoder
+
+import io.circe.generic.auto._
+import io.circe.Decoder
+import org.corerda.entities._
+import org.corerda.service.types.sparkdsl.SparkDSLImpl._
+import org.corerda.service.types.sparkdsl.SparkReaderImpl._
+import org.corerda.service.types.sparkdsl.SparkWriterImpl._
+
+object SparkDecoder {
+  private def notDefined(str: String): IllegalArgumentException = new IllegalArgumentException(s"$str is not Defined yet")
+
+  implicit def taskDecoder: Decoder[Node[innerType]] = taskCursor =>
+    taskCursor.get[String]("type") match {
+      case Right("in_fs_json") =>
+        for {
+          reader <- taskCursor.get[CustomReader]("config")
+        } yield Node(Zero, reader)
+      case Right("free") =>
+        for {
+          reader <- taskCursor.get[FreeReader]("config")
+        } yield Node(Zero, reader)
+      case Right("transformer") =>
+        for {
+          from <- taskCursor.get[String]("from")
+          transf <- taskCursor.get[TransformerCmp]("config")
+        } yield Node(One(from), transf)
+      case Right("binder") =>
+        for {
+          left <- taskCursor.get[String]("left")
+          right <- taskCursor.get[String]("right")
+          binder <- taskCursor.get[BinderCmp]("config")
+        } yield Node(Two(left, right), binder)
+      case Right("console") =>
+        for {
+          from <- taskCursor.get[String]("from")
+          writer <- taskCursor.get[ConsoleWriter]("config")
+        } yield Node(One(from), writer)
+      case Right("out_fs_json") =>
+        for {
+          from <- taskCursor.get[String]("from")
+          writer <- taskCursor.get[ConsoleWriter]("config")
+        } yield Node(One(from), writer)
+      case Right(other) => throw notDefined(other)
+    }
+}
